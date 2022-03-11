7f0e7f4364d711853701b6324ace01bf778bb750
biagolini <c.biagolini@gmail.com>
2022-03-11 18:57:20 -0300

    3 files modified

--- src/app/form-field-overview-example.ts
+++ src/app/form-field-overview-example.ts
@@ -26,9 +26,20 @@ export class FormFieldOverviewExample {
     state: [null, Validators.required],
     name: [null],
     email: [null],
+    reciveSMS: [false],
+    phone: [null],
   });
 
   ngOnInit(): void {
+    // How to subscribe to a form field change
+    this.myForm.get('reciveSMS')?.valueChanges.subscribe((value) => {
+      if (value == true) {
+        alert('You will recive SMSs');
+        
+      } else {
+        alert('You will NOT recive SMSs');
+      }      
+    });
     this.myForm.get('state')?.valueChanges.subscribe((value) => {
       if (value == 0) {
         this.myForm
@@ -38,6 +49,24 @@ export class FormFieldOverviewExample {
         this.myForm.get('email')?.clearValidators();
       }
       this.myForm.controls['email'].updateValueAndValidity();
+    });
+
+
+  }
+
+  turnPhoneValidators() { // set validators by function behavior
+    this.myForm.controls['phone'].setValidators(Validators.required);
+    this.myForm.controls['phone'].updateValueAndValidity();
+  }
+
+  onSubmmit() {
+    alert('Submmited!');
+    this.myForm.patchValue({
+      state: [null],
+      name: [null],
+      email: [null],
+      reciveSMS: [false],
+      phone: [null],
     });
   }
 }
